db275d5d4839b709acbad7403a5f4536fa76c695
Souvick Chakraborty <114563269+githubsouvick7@users.noreply.github.com>
2023-01-20 19:49:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,23 @@ import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
-  //code here
   const [inputValue, setInputValue] = useState("");
   const [square, setSquare] = useState(0);
 
-  const handlechange = (e) => {
+  const handleInputChange = (e) => {
     setInputValue(e.target.value);
     setSquare(e.target.value * e.target.value);
   };
 
   return (
-    <div id="main">
-      <ChildComponent />
-      <input id='input' onChange={handlechange} />
-      <p id='output'>{square}</p>
+    <div>
+      <ChildComponent
+        inputValue={inputValue}
+        onChange={handleInputChange}
+        square={square}
+      />
     </div>
-  )
+  );
 }
 
 
